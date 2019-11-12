570e15db1b74c9bfe15a6ff4e25903cd5fb3bcee
Lucas Zubeldia <lzubeldia@gmail.com>
2019-11-12 13:35:26 -0300

    random quote v1.2

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -75,34 +75,11 @@ function printQuote() {
    return print;
 }
 
-/***
-* `getRandomBg` function to get a random rgb number
-***/
 
-function getRandomBg(){
-  let randomBg;
-  for (var i = 1; i <= 100; i += 1) {
-     red = Math.floor(Math.random() * 256 );
-     green = Math.floor(Math.random() * 256 );
-     blue = Math.floor(Math.random() * 256 );
-     randomBg = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  } return randomBg;
-}
-
-/***
-* `changeRandomBg` function to use that number and change the body background
-***/
-
-function changeRandomBg() {
-  let randomBgc = getRandomBg();
-  document.body.style.backgroundColor = randomBgc;
-  console.log(randomBgc);
-
-}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE OF CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, changeRandomBg, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
